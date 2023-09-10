daaab676ec29be6f2076dc7c05ebbe7ad1d1c7dd
jbawsume <jbawsume@yahoo.com>
2023-09-10 21:59:50 +0800

    added argparse

--- utils_module/data.py
+++ utils_module/data.py
@@ -11,7 +11,7 @@ from torch.utils.data import Dataset
 import torchvision
 from skimage import io
 import argparse
-from custom_dataset_loader import StateFarmDataset
+from .custom_dataset_loader import StateFarmDataset
 
 
 def load_data(batch_size, dataset_name, num_works, train_params, model_params):
