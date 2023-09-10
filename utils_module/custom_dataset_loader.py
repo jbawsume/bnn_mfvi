7d7c3d74b0f9fb57e1b1559ce244eaa19375764b
jbawsume <jbawsume@yahoo.com>
2023-09-10 21:34:20 +0800

    removed boston added statefarm

---
+++ utils_module/custom_dataset_loader.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+import torchvision
+from skimage import io
+
+class StateFarmDataset(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index,1], self.annotations.iloc[index,2])
+        image = io.imread(img_path)
+        y_label_str = self.annotations.iloc[index,1]
+        y_label = torch.tensor(int(y_label_str[-1]))
+
+        if self.transform:
+            #data_norm_mean, data_norm_std = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+            data_norm_mean, data_norm_std = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+            transform = torchvision.transforms.Compose([
+                torchvision.transforms.ToPILImage(),
+                torchvision.transforms.Resize(128),
+                torchvision.transforms.ToTensor(),
+                #torchvision.transforms.Normalize(mean=data_norm_mean, std=data_norm_std)
+            ])
+            image = transform(image)
+
+        return (image, y_label)
--- utils_module/data.py
+++ utils_module/data.py
@@ -1,11 +1,16 @@
 import torch
-from sklearn.datasets import load_boston
+from custom_dataset_loader import StateFarmDataset
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import DataLoader
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST, CIFAR10
 from torchvision.transforms import Compose
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+import torchvision
+from skimage import io
 
 
 def load_data(batch_size, dataset_name, num_works, train_params, model_params):
@@ -21,24 +26,18 @@ def load_data(batch_size, dataset_name, num_works, train_params, model_params):
         model_params['in_size'] = 3
         model_params['out_size'] = 10
         train_params['save_acc'] = True
-    elif dataset_name == 'BOSTON':
-        train_set, test_set = load_boston_dataset(batch_size, num_works)
-        model_params['hin'] = 1
-        model_params['in_size'] = 13
-        model_params['out_size'] = 1
-        train_params['save_acc'] = False
+    elif dataset_name == 'STATEFARM':
+        train_set, test_set = load_statefarm_dataset(batch_size, num_works)
+        model_params['hin'] = 32
+        model_params['in_size'] = 3
+        model_params['out_size'] = 10
+        train_params['save_acc'] = True
     else:
         raise ValueError('To implement')
 
     train_params['nb_batches'] = train_set.__len__()
     train_params['p'] = train_set.dataset.__len__()
     return train_set, test_set
-
-
-def load_boston_dataset(batch_size, num_works):
-    dataset = BostonDataset()
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_works)
-    return train_loader, _
 
 
 def load_mnist_dataset(batch_size, num_works):
@@ -67,16 +66,16 @@ def load_cifar_dataset(batch_size, num_works):
     print('CIFAR LOADER')
     return train_set, test_set
 
-
-class BostonDataset(torch.utils.data.Dataset):
-    def __init__(self):
-        x, y = load_boston(return_X_y=True)
-        x = StandardScaler().fit_transform(x)
-        self.data = torch.from_numpy(x).float()
-        self.targets = torch.from_numpy(y).float()
-
-    def __len__(self):
-        return len(self.targets)
-
-    def __getitem__(self, i):
-        return self.data[i], self.targets[i]
+def load_statefarm_dataset(batch_size, num_works):
+    transform = transforms.Compose(
+            [transforms.ToPILImage(),
+                transforms.Resize(128),transforms.ToTensor()
+             ])
+    statefarm_dataset = StateFarmDataset(csv_file='/root/driver_imgs_list.csv', root_dir='/root/imgs/train',
+                                                          transform=transform)
+    train_length=int(0.7* len(statefarm_dataset))
+    test_length=len(statefarm_dataset)-train_length
+    train_dataset,test_dataset = torch.utils.data.random_split(statefarm_dataset,(train_length,test_length))
+    train_set = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_works)
+    test_set = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_works)
+    return train_set, test_set
